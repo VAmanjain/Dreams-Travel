abedbb5fc808f43427663ddbf8046619a09b3d0b
VAmanJain <vaman.jain17@gmail.com>
2023-11-05 17:25:35 +0530

    new loader add

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Loader.css'
+import { trio } from 'ldrs'
 
+trio.register()
 const Loader = () => {
   return (
 <div class=" w-full h-screen flex  justify-center items-center bg-[#033c87]">
-<div class="custom-loader"></div>
+<l-trio
+  size="80"
+  speed="1.3" 
+  color="White" 
+></l-trio>
 </div>
   )
 }
