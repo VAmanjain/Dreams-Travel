2c5959277dfd597938d2426c25d78af896776f44
VAmanJain <vaman.jain17@gmail.com>
2023-11-19 12:58:48 +0530

    add all new folder

--- src/components/Profolio/protfolio.jsx
+++ src/components/Profolio/protfolio.jsx
@@ -8,54 +8,7 @@ import image from "../../Assest/gridImage.png";
 
 const Protfolio = () => {
   return (
-    // <div className="about" name="about">
-    //   <div className=" header flex ">
-    //     <h1>About Us </h1>
-    //     <p></p>
-    //   </div>
-    //   <div className="subscribe section container">
-    //     <div className="secContainer grid">
-    //       <div className="Imge">
-    //         <img src={Image1} alt="Div Iamge" className="imge-1" />
-    //       </div>
-
-    //       <div className="textDiv">
-    //         <h4 > Who We Are</h4>
-    //         <p>
-    //           We are a team of passionate travelers, explorers, and dreamers
-    //           ourselves. We understand the profound impact of travel on personal
-    //           growth and the lasting memories it creates. Our goal is to enable
-    //           you to embark on journeys that go beyond mere vacations, enriching
-    //           your life with moments of wonder, discovery, and connection.
-    //         </p>
-    //       </div>
-    //     </div>
-    //     <div className="secContainer grid">
-    //       <div className="textDiv">
-    //         <h4>OUR MOTIVE</h4>
-    //         <p>
-    //           <ul>
-    //             <li >
-    //               <span>Inspiration</span>: We inspire you to dream bigger,
-    //               venture farther, and seek out the extraordinary.
-    //             </li>
-    //             <li>
-    //               <span>Transformation</span>:We aim to transform your travel
-    //               dreams into vivid, life-altering experiences.
-    //             </li>
-    //             <li>
-    //               <span>Memories</span>: We're here to help you craft memories
-    //               that you'll cherish for a lifetime.
-    //             </li>
-    //           </ul>
-    //         </p>
-    //       </div>
-    //       <div className="Imge">
-    //         <img src={Image2} alt="Div Iamge" style={{ width: "100%"  }} className="imge-2" />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+  
     <div className="portifolio section container" name="protfolio">
       <div className="secContainer grid">
         <div className="leftContent">
--- src/components/Subscribe/subscribe.jsx
+++ src/components/Subscribe/subscribe.jsx
@@ -9,17 +9,21 @@ const Subscribe = () => {
         <img src={Image} alt="Div Iamge" />
 
         <div className="textDiv">
-          <h4 >Best way to start your journey</h4>
-          <p >
+          <h4>Best way to start your journey</h4>
+          <p>
             We offer personalized itinearies tailored to individual perfernces
             and interests.
           </p>
           <span>Subscribe Our Newsletter</span>
           <form action="" className="form ">
             <div className="formContent flex">
-            <input type="email" placeholder="Enter Your email address" className="input" />
-          <button className="btn">Login</button>
-            </ div>
+              <input
+                type="email"
+                placeholder="Enter Your email address"
+                className="input"
+              />
+              <button className="btn">Login</button>
+            </div>
           </form>
         </div>
       </div>
