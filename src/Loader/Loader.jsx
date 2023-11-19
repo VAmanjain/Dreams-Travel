6411427a6616d5708068fc54443e8a00eccedd59
VAmanJain <vaman.jain17@gmail.com>
2023-11-19 12:47:38 +0530

    delete all new folder

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './Loader.css'
-import { trio } from 'ldrs'
+// import React from 'react'
+// import './Loader.css'
+// import { trio } from 'ldrs'
 
-trio.register()
-const Loader = () => {
-  return (
-<div class=" w-full h-screen flex  justify-center items-center bg-[#033c87]">
-<l-trio
-  size="80"
-  speed="1.3" 
-  color="White" 
-></l-trio>
-</div>
-  )
-}
+// trio.register()
+// const Loader = () => {
+//   return (
+// <div class=" w-full h-screen flex  justify-center items-center bg-[#033c87]">
+// <l-trio
+//   size="80"
+//   speed="1.3" 
+//   color="White" 
+// ></l-trio>
+// </div>
+//   )
+// }
 
-export default Loader
+// export default Loader
